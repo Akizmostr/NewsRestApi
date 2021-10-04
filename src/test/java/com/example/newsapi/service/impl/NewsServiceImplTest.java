b345976cda83515c561beef1ac1533ff7c4bcc79
Слава <nterehina666@mail.ru>
2021-10-04 21:15:38 +0300

    .

---
+++ src/test/java/com/example/newsapi/service/impl/NewsServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.example.newsapi.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith
+class NewsServiceImplTest {
+
+    @Test
+    void getAllNews() {
+    }
+}
