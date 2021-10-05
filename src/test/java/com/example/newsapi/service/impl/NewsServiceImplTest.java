9cc1f4be76a337676a61f24da3210c3a2f88f81d
Слава <nterehina666@mail.ru>
2021-10-05 14:43:39 +0300

    z

--- src/test/java/com/example/newsapi/service/impl/NewsServiceImplTest.java
+++ src/test/java/com/example/newsapi/service/impl/NewsServiceImplTest.java
@@ -1,13 +1,69 @@
 package com.example.newsapi.service.impl;
 
+import com.example.newsapi.dto.NewsDTO;
+import com.example.newsapi.entity.News;
+import com.example.newsapi.exception.ResourceNotFoundException;
+import com.example.newsapi.modelassembler.NewsModelAssembler;
+import com.example.newsapi.repository.NewsRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-@RunWith
+@ExtendWith(MockitoExtension.class)
 class NewsServiceImplTest {
+    @InjectMocks
+    NewsServiceImpl newsService;
+
+    @Mock
+    NewsRepository newsRepository;
+
+    @Mock
+    NewsModelAssembler assembler;
 
     @Test
-    void getAllNews() {
+    void whenFindNewsNotFound_thenNotFoundException() {
+        //when(newsRepository.existsById(anyLong())).thenReturn(false);
+
+        assertThrows(ResourceNotFoundException.class, () -> newsService.getNewsById(1));
+    }
+
+    @Test
+    void whenDeleteNewsNotFound_thenNotFoundException(){
+        assertThrows(ResourceNotFoundException.class, () -> newsService.deleteNewsById(1));
+    }
+
+    @Test
+    void whenUpdateNewsNotFound_thenNotFoundException(){
+        assertThrows(ResourceNotFoundException.class, () -> newsService.updateNews(null, 1, null));
+    }
+
+    @Test
+    void whenCreateValidNews_thenSuccess(){
+        News news = new News();
+        news.setDate(LocalDate.parse("2021-09-09"));
+        news.setId(1);
+        news.setText("test text");
+        news.setTitle("test title");
+        news.setComments(null);
+
+        when(newsRepository.save(news)).thenReturn(news);
+
+        NewsDTO newsDto = new NewsDTO();
+        newsDto.setDate(LocalDate.parse("2021-09-09"));
+        newsDto.setId(1);
+        newsDto.setText("test text");
+        newsDto.setTitle("test title");
+
+        NewsDTO savedNewsDto = newsService.createNews(newsDto);
+
+        assertNotNull(savedNewsDto);
+        //assertEquals();
     }
 }
